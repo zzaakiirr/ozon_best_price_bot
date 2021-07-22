bfc8c7790274dfc8e654e20d8eb21efb82156464
Zakir Dzhamaliddinov <pfrbhxbr@gmail.com>
2021-07-22 23:52:12 +0300

    Added searching for current price in page multiple times (default is 10)

--- ozon/ozon_parser.py
+++ ozon/ozon_parser.py
@@ -6,17 +6,12 @@ class OzonParser:
 
     # MARK: - Public static methods
 
-    @staticmethod
-    def find_current_price(soup):
+    @classmethod
+    def find_current_price(cls, soup):
         print('\t[INFO] Searching for current price...')
 
-        html_text = str(soup)
-
         try:
-            current_price_index = html_text.index(CURRENT_PRICE_STR)
-            start_index = current_price_index + len(CURRENT_PRICE_STR) + 1
-            end_index = html_text.index(',', current_price_index)
-            current_price = html_text[start_index:end_index]
+            current_price = cls._find_current_price(soup)
             float(current_price)
         except ValueError:
             print('\t[ERROR] Cannot parse current price\n')
@@ -25,8 +20,8 @@ class OzonParser:
         print('\t[SUCCESS] Done!\n')
         return f'{current_price}'
 
-    @staticmethod
-    def find_best_price(soup, target_str=BEST_PRICE_STR):
+    @classmethod
+    def find_best_price(cls, soup, target_str=BEST_PRICE_STR):
         print('\t[INFO] Searching for best price...')
 
         target_divs = soup.find_all('div', class_='kxa6')
@@ -44,3 +39,28 @@ class OzonParser:
 
         print('\t[WARNING] Cannot parse best price\n')
         return None
+
+    # MARK: - Private static methods
+
+    @classmethod
+    def _find_current_price(cls, soup):
+        current_price = cls._find_current_price_by_xpath(soup)
+        if current_price:
+            return current_price
+
+        html_text = str(soup)
+        return cls._find_current_price_by_keyword(html_text)
+
+    @classmethod
+    def _find_current_price_by_keyword(cls, html_text):
+        current_price_index = html_text.index(CURRENT_PRICE_STR)
+        start_index = current_price_index + len(CURRENT_PRICE_STR) + 1
+        end_index = html_text.index(',', current_price_index)
+        return html_text[start_index:end_index]
+
+    @classmethod
+    def _find_current_price_by_xpath(cls, soup):
+        target_tags = soup.select('.c2h5 span:first-child')
+        if len(target_tags) != 1 or len(target_tags[0].contents) == 0:
+            return None
+        return target_tags[0].contents[0]
